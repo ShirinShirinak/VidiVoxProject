eab9230a91c3901278ed56e38cb41fd1b3b96d0b
shirin <shei023@aucklanduni.ac.nz>
2015-10-20 13:41:39 +1300

    Multiple audios can be added to the video - fault fixes are required

--- src/functionality/MergeAudioAndVideo.java
+++ src/functionality/MergeAudioAndVideo.java
@@ -8,29 +8,47 @@ public class MergeAudioAndVideo extends SwingWorker<Void, String>{
 	String saveDes = System.getProperty("user.home") + "/temp";
 	private ProcessBuilder mergeBuilder;
 	private ProcessBuilder audioDelayBuilder;
-	
+	Process processAudioDelay;
+	Process processMerge;
+	String tempMp3s = "";
+
 	public MergeAudioAndVideo(String videoPath, Audio[] ListAudio ){
 		this.videoPath = videoPath;
 		ListAudioToMerge = ListAudio;
-		
-		
+
+
 	}
 	@Override
 	protected Void doInBackground() throws Exception {
 		String cmd = null;
+		int audioCount = 0;
 		for (int i=0; i<ListAudioToMerge.length; i++){
-			cmd = "ffmpeg -i "+ListAudioToMerge[i].getAudioPath()+" -filter_complex adelay="+ListAudioToMerge[i].getTime()+" "+ saveDes+i+".mp3";
-			audioDelayBuilder = new ProcessBuilder("/bin/bash", "-c", cmd);
-			Process processAudioDelay = audioDelayBuilder.start();
-			System.out.println("in the background task");
+			if (ListAudioToMerge[i] != null){
+				cmd = "ffmpeg -i "+ListAudioToMerge[i].getAudioPath()+" -filter_complex adelay="+ListAudioToMerge[i].getTime()+" "+ saveDes+i+".mp3";
+				audioDelayBuilder = new ProcessBuilder("/bin/bash", "-c", cmd);
+
+				System.out.println("in the background task - mp3 delay");
+				tempMp3s+=" -i "+ListAudioToMerge[i].getAudioPath();
+				audioCount++;
+				System.out.println(ListAudioToMerge.length);
+			}
+			processAudioDelay = audioDelayBuilder.start();
 		}
-		
-		
+		System.out.println(tempMp3s);
+		//processAudioDelay.waitFor();
+		System.out.println(audioCount);
+
+		cmd = "ffmpeg -i "+videoPath+tempMp3s+" -filter_complex amix=inputs="+(audioCount+1)+":duration=first /home/shirin/merged.avi";
+		mergeBuilder = new ProcessBuilder("/bin/bash","-c", cmd);
+		processMerge = mergeBuilder.start();
+		System.out.println("in the background task - merging");
+
+
 		//String cmd = "echo 'hello world' | festival --tts";
 		//mergeBuilder = new ProcessBuilder("/bin/bash", "-c", cmd);
 		//mergeBuilder = new ProcessBuilder("/bin/bash", "-c", "ffmpeg", "-i", videoPath, "-i", audioPath, "-filter_complex", "adelay="+audioTime, "amix=inputs=2", ":duration=first", saveDes);
-		
-		
+
+
 		//Process process = mergeBuilder.start();
 		return null;
 	}
